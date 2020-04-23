9d0750ac86a85f5f3b24e42c189ff7ed45335bdd
TS Lee <50777841+tbecklee@users.noreply.github.com>
2020-04-22 21:39:17 -0700

    Added all K Singhs files for portfolio

---
+++ HTML-Project-3/index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Kavya Singh, 7th Period]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">Home</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="last_aboutme.html">AboutMe</a></li></br>
+      <li><a href="last_projects.html">Projects</a></li></br>
+      <li><a href="last_resume.html">Resume</a></li></br>
+      <li><a href="last_contactme.html">ContactMe</a></li></br>
+    <ul>
+  </body>
+</html>
